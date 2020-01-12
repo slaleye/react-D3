aaa22ae36fa1ec4060b3a53104c18dce05d6260f
Salomon Laleye <salomonlaleye@gmail.com>
2020-01-13 00:23:11 +0100

    [TASK] feature/PARTONE  Installing and Adding Circles with D3

--- react-app-part-one/src/App.js
+++ react-app-part-one/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect} from 'react';
+import {select} from 'd3';
 import './App.css';
 
+const data =[10,20,30,40,50];
+
 function App() {
+    //useRef : hook used to access Dom element
+  const svgRef = useRef();
+  // useEffect will be called once the Dom element has been rendered, and also when element in the array passed to it has been changed
+  useEffect( () => {
+    console.log(svgRef);
+    const svg = select(svgRef.current);
+    svg.selectAll("circle")
+        .data(data)
+        .join(
+            onenter => onenter.append("circle").attr("class","new"),
+            onupdate => onupdate.attr("class", "updated"),
+            onexit => onexit.remove()
+            )
+    ; // D3 will create 5 elements of Circle in the svg
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <svg ref={svgRef}></svg>
   );
 }
 
