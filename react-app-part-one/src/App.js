b1f40239e67e33e7ccee30f3053d1a3797ce7ee5
Salomon Laleye <salomonlaleye@gmail.com>
2020-01-13 00:52:42 +0100

    [TASK] feature/PARTONE Add attributes to Circle elems

--- react-app-part-one/src/App.js
+++ react-app-part-one/src/App.js
@@ -1,10 +1,11 @@
-import React, { useRef, useEffect} from 'react';
+import React, { useRef, useEffect, useState} from 'react';
 import {select} from 'd3';
 import './App.css';
 
-const data =[10,20,30,40,50];
+
 
 function App() {
+   const[data, setData] = useState([10,20,30,40,50]);
     //useRef : hook used to access Dom element
   const svgRef = useRef();
   // useEffect will be called once the Dom element has been rendered, and also when element in the array passed to it has been changed
@@ -14,16 +15,24 @@ function App() {
     svg.selectAll("circle")
         .data(data)
         .join(
-            onenter => onenter.append("circle").attr("class","new"),
+            onenter => onenter.append("circle")
+                .attr("class","new")
+                .attr("r", value => value)
+                .attr("cx", value => value*2 )
+                .attr("cy", value => value*2 )
+                .attr("stroke", 'blue'),
             onupdate => onupdate.attr("class", "updated"),
             onexit => onexit.remove()
             )
     ; // D3 will create 5 elements of Circle in the svg
-  },[]);
+  },[data]);
 
-  return (
-  <svg ref={svgRef}></svg>
-  );
+  return (<React.Fragment>
+        <svg ref={svgRef}></svg>
+        <br/>
+        <button onClick={ () => setData(data.map( value => value+ 5))} >Update Data</button>
+        <button onClick={() => setData(data.filter(value => value < 35))}>Filter Data</button>
+      </React.Fragment>);
 }
 
 export default App;
