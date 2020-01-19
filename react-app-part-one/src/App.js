b31bd5762d3f0a6ad526b4c595dde23f585961ed
Salomon Laleye <salomonlaleye@gmail.com>
2020-01-19 21:00:26 +0100

    [PARTTHREE] Curved Line Chart with X and Y Axis

--- react-app-part-one/src/App.js
+++ react-app-part-one/src/App.js
@@ -1,41 +1,88 @@
 import React, { useRef, useEffect, useState} from 'react';
-import {select, line, curveCardinal} from 'd3';
+import {select, line, curveCardinal, axisBottom, axisRight, scaleLinear} from 'd3';
 import './App.css';
 
 
 
 function App() {
-  const totalSvgHeight = 150;
-  const initialData = [34,10,20,60,20,40,10];
+
+  const svgHeight = 200;
+  const svgWidth = 400;
+  const paddingLeft = 10;
+  const paddingRight = 30;
+  const paddingBottom = 20;
+  const initialData = [35,5,15,60,20,40,10,75,60,32];
+
   const [dataSet, setData] = useState(initialData);
 
   const svgRef = useRef();
 
+  // Called initially and every data chage 
   useEffect( () => {
     console.log(svgRef);
     const svg = select(svgRef.current);
-    // line generates the d element of the path based on the daa it gets
-    const myLine = line().x((value, index) => index * 50)
-                    .y(value => totalSvgHeight-value)
-                    .curve(curveCardinal); // Started from the bottom now we here XD, make chart start
-                    //from the bottom by removing them from the total height of the svg
-                    // curve on line function to make the graph a curve using curveCardinal
-    // use an array in selectAll().ddata, so D3 generates only one path element with the data inside our dataSet
-    svg.selectAll('path').data([dataSet]).join('path')
-        .attr('d', value => myLine(value))
+    // Scale needs a domain for the input value from which we scale up or down and a range
+    // To scale the index value from 0-6 of the initialData array
+    // Range of visual representation the width of the svg : 300
+    // If the value is 0 -> 0, 6 -> 300 if between  0 and 6 map to a value betwenn 0 and 300
+    const xScale = scaleLinear()
+      .domain([0, initialData.length-1])
+      .range([paddingLeft, svgWidth-paddingRight]);
+
+    const maxValue = Math.max(...initialData);
+      console.log(maxValue);
+    
+      // Domain from 0 to highest value in data, in this case svgHeight -max value
+      // Range from height of the svg :150 , to 0, 0 will be mapped to the bottom
+    const highestYValue = svgHeight - maxValue+paddingBottom
+      const yScale = scaleLinear()
+                    .domain([0,highestYValue])
+                    .range([svgHeight-paddingBottom, 0]);  
+
+    // Axis Bottom: simply places the ticks at the bottom, but does not automatically place the axes at the bottom              
+    const xAxis = axisBottom(xScale)
+                  .ticks(dataSet.length)
+                  .tickFormat(index => index+1); // Expects a scale
+
+ // Equivalent to xAxis(svg.select(".x-axis"))
+    svg.select(".x-axis")
+        .style('transform',` translateY(${svgHeight-paddingBottom}px)` )
+        .call(xAxis);
+ 
+   // Add Right Axis
+   const yAxis = axisRight(yScale);
+   svg.select(".y-axis")
+            .style('transform',` translateX(${svgWidth-paddingRight}px)` )
+            .call(yAxis);
+
+
+    // Generates "d" element for "path" element 
+    const myLine = line().x((value, index) =>xScale(index))
+                    .y(yScale) // equivalent to value => yScale(value)
+                    .curve(curveCardinal);
+                    
+    svg.selectAll('.line') //When xaxis bottom added the graph rendered at the bottom use 'line'
+        .data([dataSet])
+        .join('path')
+        .attr('class','line')
+        .attr('d', myLine ) // equivalent tovalue => myLine(value)
         .attr('fill','none')
         .attr('stroke','orange');
 
-  },[dataSet]);
+  },[dataSet,initialData]);
 
   return (<React.Fragment>
-        <h2>Line Chart</h2>
-        <svg ref={svgRef}>
+        <h2>Curved Line Chart: X and Y Axis</h2>
+        <svg width={svgWidth} height={svgHeight} ref={svgRef}>
+          <g className="x-axis"/>
+          <g className="y-axis"/>
         </svg>
         <br/>
         <button onClick={ () => setData(dataSet.map( value => value+ 5))} >Update + 5</button>
         <span>&nbsp;</span>
-  <button onClick={() => setData(dataSet.filter(value => value < 35))}>Filter {"<"} 35</button>
+        <button onClick={() => setData(dataSet.filter(value => value > 30))}>Filter {">"} 30</button>
+        <span>&nbsp;</span>
+        <button onClick={() => setData(dataSet.filter(value => value < 30))}>Filter {"<"} 30</button>
         <span>&nbsp;</span>
         <button onClick={() => setData(initialData.map(value => value ))}>Reset </button>
       </React.Fragment>);
